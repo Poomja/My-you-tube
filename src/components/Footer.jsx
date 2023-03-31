9ce825798b9909e1085c04ac6fdaa594ed5b2063
Pooja Sonje <108693951+Poomja@users.noreply.github.com>
2023-04-01 04:05:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
       <Box sx={{ mt: 'auto', p: '0.3rem', bgcolor: '#ebb9b9', zIndex:1201 }}>
         <Typography sx={{ textAlign: 'center', verticalAlign: 'middle' }}>
-          Made with love by <a style={{textDecoration: 'none'}} target="_blank" href="https://github.com/Poomja">Sudhanva</a>
+          Made with love by <a style={{textDecoration: 'none'}} target="_blank" href="https://github.com/Poomja">POOJA</a>
         </Typography>
       </Box>
   )
