34baa42e8f3a5017217adf46130067dcb6027751
Pooja Sonje <108693951+Poomja@users.noreply.github.com>
2023-04-04 00:23:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {Box, Typography} from "@mui/material";
 
 const Footer = () => {
   return (
-      <Box sx={{ mt: 'auto', p: '0.3rem', bgcolor: '#ebb9b9', zIndex:1201 }}>
+      <Box sx={{ mt: 'auto', p: '0.3rem', bgcolor: '#f5d7d7', zIndex:1201 }}>
         <Typography sx={{ textAlign: 'center', verticalAlign: 'middle' }}>
           Made with love by <a style={{textDecoration: 'none'}} target="_blank" href="https://github.com/Poomja">POOJA</a>
         </Typography>
